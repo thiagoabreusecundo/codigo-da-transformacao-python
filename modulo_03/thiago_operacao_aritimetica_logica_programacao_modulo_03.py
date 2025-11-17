7976c49daf1f78a8560ca3c66adbedd7a70ddfa1
thiagoabreusecundo <thiagogamesfreefire@gmail.com>
2025-11-17 15:17:20 -0300

    79

---
+++ modulo_03/thiago_operacao_aritimetica_logica_programacao_modulo_03.py
@@ -0,0 +1,13 @@
+print("-------------")
+print("bem vindo a cauculadora")
+print("-----------")
+
+
+n1= int(input("insira um nome"))
+n2 = int(input("insira outro numero"))
+
+adicao = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1/n2
+
